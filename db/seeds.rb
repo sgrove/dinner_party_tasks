661acfc0d4acce8c8ec86f644080d672eb559ee2
Amy Chen <lemur.amychen@gmail.com>
2010-03-03 20:26:46 -0800

    added seeds file; use local jquery and jquery-ui

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+
+Task.create(:title=>"Mix pizza dough", :active=>true, :minutes=>15)
+Task.create(:title=>"Make sauce", :active=>true, :minutes=>30)
+Task.create(:title=>"Brown sausage", :active=>true, :minutes=>10)
+Task.create(:title=>"Grate cheese", :active=>true, :minutes=>5)
+Task.create(:title=>"Let pizza dough rest", :active=>false, :minutes=>120)
+Task.create(:title=>"Bake pizza", :active=>true, :minutes=>30)
