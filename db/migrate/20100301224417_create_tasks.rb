f07d2e8d8e2a21b22e38d599caefd696e0c826c7
Amy Chen <lemur.amychen@gmail.com>
2010-03-03 19:38:13 -0800

    fixed .gitignore -- now I have the migrations

---
+++ db/migrate/20100301224417_create_tasks.rb
@@ -0,0 +1,17 @@
+class CreateTasks < ActiveRecord::Migration
+  def self.up
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.integer :minutes, :default=>0
+      t.boolean :active
+      t.datetime :finished_at
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :tasks
+  end
+end
